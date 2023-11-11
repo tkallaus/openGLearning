8ad85aaa19bcfb284acaf799d9f163fff6b2d2f0
tkallaus <tkallaus@asu.edu>
2023-11-10 18:00:08 -0700

    A triangle, and a lot of notes

--- UnoGL/UnoGL/Source1.cpp
+++ UnoGL/UnoGL/Source1.cpp
@@ -9,14 +9,27 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+//A temporary(hopefully) representation of a GLSL shader, bit ugly
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
 int main()
 {
 	glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //For mac i guess
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
@@ -25,7 +38,9 @@ int main()
         glfwTerminate();
         return -1;
     }
+    //Register Callbacks before the render loop and after the window's creation
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -33,11 +48,94 @@ int main()
         return -1;
     }
 
-    //Not needed cause the callback immediately sets the size to the window size
-    //glViewport(0, 0, 800, 600);
+    //BasicVertexShaderCreation
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
-    //Register Callbacks before the render loop and after the window's creation
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    //Check if the vertex compilation broke
+    /*
+    int  vsuccess;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vsuccess);
+    if (!vsuccess)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    */
+
+    //BasicFragmentShaderCreation
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    //Check if the fragment compilation broke
+    /*
+    int  fsuccess;
+    char infoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fsuccess);
+    if (!fsuccess)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    */
+
+    //Link shaders to one shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    //Check if the shader program blew up
+    /*
+    int  psuccess;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &psuccess);
+    if (!psuccess)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    */
+
+    //Delete the shaders now since they're in the program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //First Triangle
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    //Passing triangle vertex info to the graphics card, no shaders yet
+    unsigned int VBO;
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    //Bind order: Vertex Array, Vertex buffer(s), Vertex attribute(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /*Tells OpenGL how to interpret vertex data :
+    0 matchs the layout location specified in the vertex shader source
+    3 is the size of each attribute, it's a vector3.
+    GL_FLOAT is the type of each coordinate.
+    GL_FALSE specifies that we don't want the data normalized.
+    3 * sizeof(float) is the stride, or the total space between vertex attributes. There are 3 floats.
+    (void*)0 is the offset of where the data begins, it is 0 because we're handing it the data plainly.
+    */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     //Render loop, each iteration a frame
     while (!glfwWindowShouldClose(window))
@@ -48,6 +146,11 @@ int main()
         //rendering commands
         glClearColor(0.1f, 0.4f, 0.4f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //Draw
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); //Technically not needed without a second VAO
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         //check+call events, swapping buffers prevents users from seeing the image while its being drawn(?)
         glfwSwapBuffers(window);
